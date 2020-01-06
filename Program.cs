32723d0813f5a2d54b1f8be04e35b8abf5965dc7
hema M <hema_be@live.com>
2020-01-07 01:42:41 +0530

    added a line in case f in program.cs file

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void getPrefix(string StudName)
+        {
+            Console.WriteLine("Enter the Gender");
+            char gender = Convert.ToChar(Console.ReadLine().ToUpper());
+            switch (gender.ToString())
+            {
+                case "F":
+                    Console.WriteLine($"Ms.{StudName}");
+                    break;
+                case "M":
+                    Console.WriteLine($"Mr.{StudName}");
+                    break;
+                default:
+                    Console.WriteLine("Please enter the gender");
+                    break;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            //string[] StudentName = new string[10] { "Ishwariya", "Soniya", "Yuvarani", "Pricilla","Hema","Meenu","Arul","Mukesh","Shanu","Rohini"};
+            string[] StudentName = new string[10];
+            int i;
+            int j = 1;
+            Console.WriteLine("Enter 10 Student Names one by one:");
+            for (i = 0; i < 10; i++)
+            {
+                //Console.WriteLine("Enter 10 Student Names one by one:");
+                Console.WriteLine($"Enter Detail of Student {j++}:");
+                Console.WriteLine($"Enter name of Student:");
+                StudentName[i] = Console.ReadLine();
+                getPrefix(StudentName[i]);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
